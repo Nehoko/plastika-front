353fb47bbed3acd7b24845ef3b75140d4ff8a0db
IDMikhailov <idmikhailov@lanit.ru>
2019-12-05 17:38:45 +0300

    WIP

--- public/common/plastikaApp.js
+++ public/common/plastikaApp.js
@@ -1,31 +1,13 @@
-var router = require('angular-route');
+require('angular');
 
 //init angular main module
-var plastikaApp = angular.module('App', [
-    'ui.bootstrap',
-    'ngRoute'
-]);
-
-plastikaApp.service('Api', [$http, ApiService]);
-
-if (getParameterByName("token") !== "") {
-    sessionStorage.setItem("token", getParameterByName("token"));
-    location.search = "";
-}
-
-function getParameterByName(name) {
-    name = name.replace(/[\[]/,"\\[").replace(/[\]]/, "\\");
-    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-        results = regex.exec(location.search);
-    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
-
-var configFunction = function ($routeProvider, $httpProvider) {
-    $routeProvider
-        .when('grid', {
-            templateUrl
-        })
-};
+angular.module('plastikaApp', [
+    'bootstrap',
+    'angular-route'
+])
+    .controller('headerCtrl', function($scope) {
+        $scope.name = "Plastika";
+    });
 
 angular.bootstrap(document, ['angular.bootstrap'], {
     strictDi: true
--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,32 @@ const path = require('path');
 
 module.exports = {
     mode: "development",
-    entry: './server.js',
+    entry: './www.js',
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, 'dist')
     },
     externals: ["fs"],
-    node: {
-        net: 'empty',
-        tls: 'empty',
-        dns: 'empty',
-    },
     resolve: {
         alias: {
             stylus$: 'stylus/lib/stylus',
         },
+        modules: ['node_modules'],
+        extensions: ['.json','.js','.jsx'],
     },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.styl$/,
+                use: ['style-loader', 'stylus-loader']
+            }
+        ]
+    }
 };
