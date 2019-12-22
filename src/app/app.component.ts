a2f9d9c6723732bb6cccd09f559156f1545e964b
IDmikhailov <fun4lsd@gmail.com>
2019-12-22 17:50:51 +0300

    header edited

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, NgModule} from '@angular/core';
+import {Component} from '@angular/core';
 import {HeaderComponent} from './core/header/header.component';
 import {FooterComponent} from './core/footer/footer.component';
 
