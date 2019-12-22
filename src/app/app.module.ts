d607a128538e4f701d40f4c1eb6ec0be020bc62a
IDmikhailov <fun4lsd@gmail.com>
2019-12-22 19:11:17 +0300

    Добавлена валидная анимация

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -9,6 +8,7 @@ import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
 import {FormsModule} from '@angular/forms';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 @NgModule({
   declarations: [
@@ -21,8 +21,8 @@ import {FormsModule} from '@angular/forms';
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    NgbModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CollapseModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
